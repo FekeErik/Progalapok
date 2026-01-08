32220c627edc8f572025bad72a7325ba285a22fe
feke.erik <feke.erik@ckik.loc>
2026-01-08 10:35:46 +0100

    Okosvagyok

--- 2025.01.08 hazimegoldasa.py
+++ 2025.01.08 hazimegoldasa.py
@@ -21,7 +21,48 @@ for elem in lista:
 atlag=osszeg/n
 print(round(atlag,2))
 
+dba = 0
+for index in range(0,n,1):
+    #if(atlag>lista[index]):
+    if lista[index]<atlag:
+        dba+=1
+print("számtani átlag alatti értékek száma:",dba)
+
+
 szorzat = 1
 for elem in lista:
     szorzat *= elem
 matlag=math.pow(szorzat,1/n)
+
+#mértani átlag alatti számok összege
+mossz=0
+for a in lista:
+    if(matlag>a):
+        mossz+=a
+print("Mértani átlag alatti számok összege: ",mossz)
+
+
+szoveg=input("Adjon meg egy szöveget: ")
+print(szoveg)
+betu = input("Adjon meg egy betűt: ")
+
+dbv=0
+for karakter in szoveg:
+    if karakter==betu:
+        dbv+=1
+print(dbv)
+
+szo1=input("Adjon meg egy szöveget: ")
+szo2=input("Adjon meg egy szöveget: ")
+
+minimumhossz=0
+kulonbseg=0
+
+if(len(szo1)>len(szo2)):
+    minimumhossz=len(szo2)
+else:
+    minimumhossz=len(szo1)
+for i in range(0,minimumhossz,1):
+    if (szo1[i] !=szo2[i]):
+        kulonbseg+=1
+print(kulonbseg)
